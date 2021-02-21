942165652f2ed7047210a4936b7703dec4dd3807
Arthur Kazemi <bidadh@gmail.com>
2021-02-22 00:38:19 +1100

    add and configure http token interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,15 @@ import {CommonModule} from '@angular/common';
 import {MainLayoutComponent} from './layout/main-layout/main-layout.component';
 import {BrowserModule} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {OAuthModule} from "angular-oauth2-oidc";
+import {HttpTokenInterceptor} from "./http.token.interceptor";
 
 
 @NgModule({
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
+  ],
   declarations: [MainLayoutComponent],
   imports: [
     HttpClientModule,
