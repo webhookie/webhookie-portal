38da4dcad3f741f183abe581795f2824c09d9784
vision <visionvivante@gmail.com>
2021-03-04 13:18:59 +0530

    commitCode

--- src/app/components/webhooks/webhooks.component.ts
+++ src/app/components/webhooks/webhooks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-webhooks',
   templateUrl: './webhooks.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebhooksComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    $(document).ready(function () {
+      
+      $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+     });
+    });
+   }
 
   ngOnInit(): void {
+   
   }
 
 }
