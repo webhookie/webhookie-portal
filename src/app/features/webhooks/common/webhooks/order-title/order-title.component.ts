12260e60a1d318453b67ec04d08eb5206f52f845
vision vivante <visionvivante@gmail.com>
2021-03-20 18:33:03 +0530

    20MarchChanges

--- src/app/features/webhooks/common/webhooks/order-title/order-title.component.ts
+++ src/app/features/webhooks/common/webhooks/order-title/order-title.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { VariableService } from '../../variable.service';
 @Component({
   selector: 'app-order-title',
   templateUrl: './order-title.component.html',
   styleUrls: ['./order-title.component.css']
 })
 export class OrderTitleComponent implements OnInit {
+  constructor(private variable:VariableService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  title(){
+    let crumbs=this.variable.breadCrumbs();
+    return crumbs[crumbs.length-1].displayName;
   }
 
 }
