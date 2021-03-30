6d9b88ca068366562fb2bd920ebe253a941735f2
Arthur Kazemi <bidadh@gmail.com>
2021-03-31 10:46:46 +1100

    read applications

--- src/app/features/webhooks/subscribe-webhook/application/application.component.ts
+++ src/app/features/webhooks/subscribe-webhook/application/application.component.ts
@@ -1,43 +1,61 @@
-import { Component, OnInit ,TemplateRef,Input} from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal'; 
-import{VariableService}from 'src/app/features/webhooks/common/variable.service';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
+import {VariableService} from 'src/app/features/webhooks/common/variable.service';
 import * as $ from 'jquery';
+import {ApplicationService} from "../../service/application.service";
+import {LogService} from "../../../../shared/log.service";
+import {ReplaySubject, Subject} from "rxjs";
+import {Application} from "../../model/application";
+
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.css']
 })
 export class ApplicationComponent implements OnInit {
-  appName:any;
-  Desc:any;
+  appName: any;
+  Desc: any;
   appSelected: any;
+
+  readonly _applications$: Subject<Array<Application>> = new ReplaySubject();
+
   constructor(public modalRef: BsModalRef,
-    private modalService: BsModalService,
-    public variable: VariableService
-    ) { 
-      this.variable.app=false;
-    }
+              private readonly applicationService: ApplicationService,
+              private readonly log: LogService,
+              private modalService: BsModalService,
+              public variable: VariableService
+  ) {
+    this.variable.app = false;
+  }
 
   ngOnInit(): void {
     $(document).ready(function () {
-      $(".btn-warning").click(function () { 
+      $(".btn-warning").click(function () {
         $(this).toggleClass("active").parent().parent().siblings().find('after').removeClass('active')
       });
     })
 
-    
-  }
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template,{class:'modal-dialog-centered modal-w',backdrop: true,ignoreBackdropClick: true });    
+    this.applicationService.myApplications()
+      .subscribe(it => this._applications$.next(it));
+
   }
 
-  selectApp(val:any){
-    this.variable.appName=this.appSelected=val;
-    this.variable.app=true;
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {
+      class: 'modal-dialog-centered modal-w',
+      backdrop: true,
+      ignoreBackdropClick: true
+    });
   }
-  create(){
-    this.variable.appName=this.appSelected='Volvo cars';
-    this.variable.app=true;
+
+  selectApp(val: Application) {
+    this.variable.appName = this.appSelected = val.name;
+    this.variable.app = true;
+  }
+
+  create() {
+    this.variable.appName = this.appSelected = 'Volvo cars';
+    this.variable.app = true;
     this.modalRef.hide();
   }
 }
