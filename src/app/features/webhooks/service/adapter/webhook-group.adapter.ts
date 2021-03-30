d335a6d83380a876dcd74fbc3450185f8404ec7f
Arthur Kazemi <bidadh@gmail.com>
2021-03-31 10:46:29 +1100

    minor refactor

--- src/app/features/webhooks/service/adapter/webhook-group.adapter.ts
+++ src/app/features/webhooks/service/adapter/webhook-group.adapter.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Adapter} from "../../../shared/adapter/adapter";
-import {Topic, WebhookGroup} from "../model/webhook-group";
+import {Adapter} from "../../../../shared/adapter/adapter";
+import {Topic, WebhookGroup} from "../../model/webhook-group";
 
 @Injectable({
   providedIn: 'root'
