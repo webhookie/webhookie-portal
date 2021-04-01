cfc0c1b382092a443ede5cd6ace20738ccf95e4b
Arthur Kazemi <bidadh@gmail.com>
2021-04-01 18:25:15 +1100

    read callback & topic subscription and update buttons

--- src/app/features/webhooks/subscribe-webhook/subscribe-webhook.component.ts
+++ src/app/features/webhooks/subscribe-webhook/subscribe-webhook.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {BsModalService} from 'ngx-bootstrap/modal';
-import {VariableService} from "../common/variable.service";
 import {WebhooksContext} from "../webhooks-context";
 import {ResponseComponent} from "../common/response/response.component";
 import {ApplicationComponent} from "./application/application.component";
 import {CallbackComponent} from "./callback/callback.component";
 import {CallbackService, CallbackValidationRequest} from "../service/callback.service";
 import {RequestExampleComponent} from "../common/request-example/request-example.component";
+import {mergeMap} from "rxjs/operators";
+import {SubscriptionService} from "../../../shared/subscription.service";
+import {Observable} from "rxjs";
+import {Subscription} from "../../../shared/model/subscription";
 
 @Component({
   selector: 'app-subscribe-webhook',
@@ -20,17 +22,30 @@ export class SubscribeWebhookComponent implements OnInit {
   @ViewChild('responseComponent') response?: ResponseComponent
   @ViewChild('requestExampleComponent') requestComponent?: RequestExampleComponent
 
+  subscription?: Subscription
+
   constructor(
-    readonly variable: VariableService,
-    private modalService: BsModalService,
     private readonly context: WebhooksContext,
+    private readonly subscriptionService: SubscriptionService,
     private readonly callbackService: CallbackService
   ) {
   }
 
+  private fetchSubscriptions(callbackId: string): Observable<Array<Subscription>> {
+    return this.subscriptionService.fetchSubscriptions("CONSUMER", this.context.selectedTopic?.name, callbackId)
+  }
+
   ngOnInit(): void {
     this.context.selectedCallback$
-      .subscribe(() => this.response?.invalidate())
+      .pipe(
+        mergeMap(it => this.fetchSubscriptions(it.callbackId))
+      )
+      .subscribe(it => {
+        if(it.length > 0) {
+          this.subscription = it[0]
+        }
+        this.response?.invalidate()
+      })
   }
 
   title() {
@@ -39,21 +54,24 @@ export class SubscribeWebhookComponent implements OnInit {
 
   test() {
     this.response?.invalidate()
-    let callback = this.context.currentCallback
+    this.context.selectedCallback$
+      .pipe(
+        mergeMap(it => {
+          let request: CallbackValidationRequest = {
+            httpMethod: it.httpMethod,
+            url: it.url,
+            payload: JSON.stringify(this.requestComponent?.jsonobj?.result),
+            headers: {
+              "Content-Type": ["application/json"],
+              "Accept": ["*/*"]
+            },
+            traceId: "1",
+            spanId: "1"
+          };
 
-    let request: CallbackValidationRequest = {
-      httpMethod: callback.httpMethod,
-      url: callback.url,
-      payload: JSON.stringify(this.requestComponent?.jsonobj?.result),
-      headers: {
-        "Content-Type": ["application/json"],
-        "Accept": ["*/*"]
-      },
-      traceId: "1",
-      spanId: "1"
-    };
-
-    this.callbackService.testCallback(request)
+          return this.callbackService.testCallback(request)
+        })
+      )
       .subscribe(it => {
         this.response?.update(it)
       })
@@ -61,6 +79,18 @@ export class SubscribeWebhookComponent implements OnInit {
 
   get selectedCallback() {
     return this.context.currentCallback
+  }
+
+  get selectedSubscription() {
+    return this.subscription
+  }
+
+  get canBeSaved() {
+    return this.selectedCallback && !this.selectedSubscription
+  }
+
+  get canBeValidated() {
+    return this.subscription?.canBeValidated()
   }
 
   get hasResponse() {
