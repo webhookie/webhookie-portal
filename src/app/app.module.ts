e18d810860b587fe47e150334a0f12c8a5a10d5d
Arthur Kazemi <bidadh@gmail.com>
2021-04-12 14:05:08 +1000

    add CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -35,6 +35,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BsDatepickerModule.forRoot()
     // ToastrModule.forRoot(),
     // BrowserAnimationsModule,
+  ],
+
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
 
   providers: [BsModalRef,BsDatepickerConfig],
