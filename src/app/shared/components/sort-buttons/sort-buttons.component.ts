3ca004917e8e7551f41a79f925a181513f3a86f4
vision <visionvivante@gmail.com>
2021-04-13 19:10:13 +0530

    sort button

---
+++ src/app/shared/components/sort-buttons/sort-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-buttons',
+  templateUrl: './sort-buttons.component.html',
+  styleUrls: ['./sort-buttons.component.css']
+})
+export class SortButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
