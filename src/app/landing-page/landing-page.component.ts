3638d188eac858902385db4d6bb719abdbdb52cc
Arthur Kazemi <bidadh@gmail.com>
2021-04-21 02:03:49 +1000

    reload webhook groups when login

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {WebhookGroupService} from "../features/webhooks/service/webhook-group.service";
 import {Observable, ReplaySubject, Subject} from "rxjs";
 import {WebhookGroup} from "../features/webhooks/model/webhook-group";
-import {map, tap} from "rxjs/operators";
+import {map, mergeMap, tap} from "rxjs/operators";
+import {ApplicationContext} from "../shared/application.context";
 
 @Component({
   selector: 'app-landing-page',
@@ -15,10 +16,18 @@ export class LandingPageComponent implements OnInit {
   showEmptyBox: boolean = false;
 
   constructor(
+    private readonly appContext: ApplicationContext,
     private readonly service: WebhookGroupService
   ) { }
 
   ngOnInit(): void {
+    this.appContext.isLoggedIn
+      .subscribe(() => this.readWebhookGroups());
+
+    this.readWebhookGroups();
+  }
+
+  private readWebhookGroups() {
     this.service.myWebhookGroups()
       .pipe(tap(it => this.showEmptyBox = it.length == 0))
       .subscribe(it => this._webhookGroups.next(it));
