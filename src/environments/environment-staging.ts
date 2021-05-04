7d061415cc6f099894387ce7677a48d44413c540
Arthur Kazemi <bidadh@gmail.com>
2021-05-04 15:05:09 +1000

    add staging env

---
+++ src/environments/environment-staging.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  mock: false,
+  apiUrl: window.location.protocol + "//" + window.location.host + "/api",
+  iam: {
+    redirectUri: window.location.protocol + "//" + window.location.host,
+    requireHttps: false,
+    showDebugInformation: true,
+    disableAtHashCheck: true
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
