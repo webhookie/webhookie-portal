1bf8f3893d44939027081d21a16b7473aa865e64
Arthur Kazemi <bidadh@gmail.com>
2021-05-18 13:30:15 +1000

    refactor and add DRAFT sampler code

--- src/app/shared/service/asyncapi-parser.service.ts
+++ src/app/shared/service/asyncapi-parser.service.ts
@@ -1,17 +1,41 @@
 import { Injectable } from '@angular/core';
 import '@asyncapi/parser/dist/bundle';
+import {AsyncAPIDocument, Schema} from "@asyncapi/parser/dist/bundle";
+import {from, Observable} from "rxjs";
+import { sample } from 'openapi-sampler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsyncapiParserService {
   // @ts-ignore
-  private parser = window.AsyncAPIParser
+  private parser: AsyncAPIParser = window.AsyncAPIParser
 
   constructor() { }
 
-  parse(spec: string): Promise<any> {
-    return this.parser
-      .parse(spec);
+  parse(spec: string): Observable<AsyncAPIDocument> {
+    return from(this.parser.parse(spec));
   }
+
+  sample(schema: Schema, it: AsyncAPIDocument): any {
+    let schema1 = {
+      type: schema.type(),
+      properties: {
+        a: {type: 'integer', minimum: 10},
+        b: {type: 'string', format: 'password', minLength: 10},
+        c: {type: 'boolean', readOnly: true}
+      }
+    };
+    return sample(schema, {skipReadOnly: true});
+    // return sample(schema, undefined, it);
+  }
+}
+
+export interface AsyncAPIParser {
+  parse(asyncapiYAMLorJSON: string, options?: {
+    path?: string;
+    parse?: any;
+    resolve?: any;
+    applyTraits?: any;
+  }): Promise<AsyncAPIDocument>;
 }
