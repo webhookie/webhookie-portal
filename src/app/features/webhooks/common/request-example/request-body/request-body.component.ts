dca3882442acb99a4ba55d8b7052667742f0f19d
Arthur Kazemi <bidadh@gmail.com>
2021-05-19 13:40:30 +1000

    refactor to WebhookBaseComponent

--- src/app/features/webhooks/common/request-example/request-body/request-body.component.ts
+++ src/app/features/webhooks/common/request-example/request-body/request-body.component.ts
@@ -1,48 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewChecked, Component} from '@angular/core';
 import * as $ from 'jquery';
 import {JsonUtils} from "../../../../../shared/json-utils";
+import {WebhookBaseComponent} from "../../webhook-base-component";
+import {WebhooksContext} from "../../../webhooks-context";
 
 @Component({
   selector: 'app-request-body',
   templateUrl: './request-body.component.html',
   styleUrls: ['./request-body.component.css']
 })
-export class RequestBodyComponent implements OnInit {
-  jsonobj = {
-    "outcome": "success",
-    "result": {
-      "name": "messaging-sockets",
-      "default-interface": "external",
-      "include": [],
-      "socket-binding": {
-        "messaging": {
-          "name": "messaging",
-          "interface": null,
-          "port": 5445,
-          "fixed-port": null,
-          "multicast-address": null,
-          "multicast-port": null
-        },
-        "messaging-throughput": {
-          "name": "messaging-throughput",
-          "interface": null,
-          "port": 5455,
-          "fixed-port": null,
-          "multicast-address": null,
-          "multicast-port": null
-        }
-      }
-    },
-    "compensating-operation": null
-  };
-
-  constructor() {}
+export class RequestBodyComponent extends WebhookBaseComponent implements AfterViewChecked {
+  constructor(ctx: WebhooksContext) {
+    super(ctx)
+  }
 
   ngOnInit(): void {
 
   }
+
   ngAfterViewChecked(){
-    const str = JSON.stringify(this.jsonobj, null, '\t');
+    const str = JSON.stringify(this.webhook.example, null, '\t');
     this.output(JsonUtils.syntaxHighlight(str));
     $(".key").attr('contentEditable', 'false');
     $(".boolean").attr('contentEditable', 'true');
