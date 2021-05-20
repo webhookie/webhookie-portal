651ade6560e9ee6b3f68fb9f25741fb1200238a7
Arthur Kazemi <bidadh@gmail.com>
2021-05-20 15:16:44 +1000

    minor fix

--- src/app/features/webhooks/webhook-page/details/webhook/webhook-detail/headers/headers.component.ts
+++ src/app/features/webhooks/webhook-page/details/webhook/webhook-detail/headers/headers.component.ts
@@ -1,11 +1,23 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Webhook} from "../../../../../model/webhook";
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-headers',
   templateUrl: './headers.component.html',
   styleUrls: ['./headers.component.css']
 })
-export class HeadersComponent {
+export class HeadersComponent implements OnInit{
   @Input() webhook!: Webhook
+
+  ngOnInit(): void {
+    $(function() {
+      $(".collapsed").on("click", function () {
+        $(this)
+          .toggleClass('active')
+          .parent().parent().siblings().find('a')
+          .removeClass('active')
+      });
+    });
+  }
 }
