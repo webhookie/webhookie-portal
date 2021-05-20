d0c3849dc22de5effb2476b651f85dec7b0a2f63
Arthur Kazemi <bidadh@gmail.com>
2021-05-20 15:27:14 +1000

    minor fixes

--- src/app/features/webhooks/webhook-page/details/webhook/webhook-detail/message-payload/message-payload.component.ts
+++ src/app/features/webhooks/webhook-page/details/webhook/webhook-detail/message-payload/message-payload.component.ts
@@ -1,13 +1,22 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MessagePayload} from "../../../../../model/message-payload";
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-message-payload',
   templateUrl: './message-payload.component.html',
   styleUrls: ['./message-payload.component.css']
 })
-export class MessagePayloadComponent {
+export class MessagePayloadComponent implements OnInit {
   @Input() payload!: MessagePayload
   @Input() index: number = 0;
   @Input() parentId!: string
+
+  ngOnInit(): void {
+    $(function() {
+      $(".body-accordion > div > div > a").on("click", function () {
+        $(this).toggleClass('active')
+      });
+    });
+  }
 }
