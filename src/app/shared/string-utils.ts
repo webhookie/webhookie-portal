55c1ec235437b25f773dcdf7001e1ca222e9a409
Arthur Kazemi <bidadh@gmail.com>
2021-07-02 00:54:04 +1000

    minor change

--- src/app/shared/string-utils.ts
+++ src/app/shared/string-utils.ts
@@ -2,7 +2,7 @@ export class StringUtils {
   static truncatedUUID(id: string): string {
     let first = id.indexOf("-") + 1;
     let last = id.lastIndexOf("-");
-    return id.substr(0, first) + "****" + id.substr(last, id.length);
+    return id.substr(0, first) + "...." + id.substr(last, id.length);
   }
 
   static encode(str: string): string {
